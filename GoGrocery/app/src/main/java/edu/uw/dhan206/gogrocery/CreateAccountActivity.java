18a4619750b8f7209e5670d697be83304a13b563
DereK Han <derekhan206@gmail.com>
2017-05-22 13:32:45 -0700

    Adds working create an account

--- GoGrocery/app/src/main/java/edu/uw/dhan206/gogrocery/CreateAccountActivity.java
+++ GoGrocery/app/src/main/java/edu/uw/dhan206/gogrocery/CreateAccountActivity.java
@@ -1,6 +1,8 @@
 package edu.uw.dhan206.gogrocery;
 
 import android.app.ActionBar;
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,33 +11,65 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
     private final static String TAG = "CreateAccountActivity";
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
 
+        mAuth = FirebaseAuth.getInstance();
+
         Button createAccount = (Button) findViewById(R.id.createAccountCreateButton);
         createAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String accountEmail = ((EditText) findViewById(R.id.createAccountEmail)).getText().toString();
+                String accountName = ((EditText) findViewById(R.id.createAccountName)).getText().toString();
                 String passwordFirst = ((EditText) findViewById(R.id.createAccountPasswordFieldFirst)).getText().toString();
                 String passwordSecond = ((EditText) findViewById(R.id.createAccountPasswordFieldSecond)).getText().toString();
 
                 if(passwordFirst.equals(passwordSecond)) {
-                    Log.v(TAG, "passwords match");
+                    mAuth.createUserWithEmailAndPassword(accountEmail, passwordFirst)
+                            .addOnCompleteListener(CreateAccountActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()) {
+                                        FirebaseUser user = mAuth.getCurrentUser();
+//                                        updateUI(user);
+
+                                        Toast.makeText(CreateAccountActivity.this, "Account successfully created.",
+                                                Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Toast.makeText(CreateAccountActivity.this, "Account creation failed.",
+                                                Toast.LENGTH_SHORT).show();
+//                                        updateUI(null);
+                                    }
+                                }
+                            });
+
                 } else {
-                    Log.v(TAG, "passwords DONT match");
-//                    AlertDialog.Builder builder = new AlertDialog.Builder(getBaseContext());
-//                    builder.setTitle("Passwords Do Not Match.");
-//                    builder.setMessage("Please try again.");
-//                    AlertDialog dialog = builder.create();
-//                    dialog.show();
+                    AlertDialog.Builder builder = new AlertDialog.Builder(CreateAccountActivity.this);
+                    builder.setTitle("Passwords Do Not Match.")
+                            .setMessage("Please try again.")
+                            .setPositiveButton("RETRY", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    // Log.v(TAG, "Close dialog");
+                                }
+                            });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
                 }
             }
         });
