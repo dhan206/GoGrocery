be27aaed59ec1fa6c11fd728962ae9ec6e48bef2
Colin Bartels <colin.0011@hotmail.com>
2017-05-22 13:14:35 -0700

    save before switch

--- GoGrocery/app/src/main/java/edu/uw/dhan206/gogrocery/ListActivity.java
+++ GoGrocery/app/src/main/java/edu/uw/dhan206/gogrocery/ListActivity.java
@@ -1,20 +1,36 @@
 package edu.uw.dhan206.gogrocery;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class ListActivity extends AppCompatActivity {
+
+    FirebaseDatabase database;
+    DatabaseReference users;
+    DatabaseReference currentList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        database = FirebaseDatabase.getInstance();
+        users = database.getReference("users");
+        currentList = database.getReference();
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowTitleEnabled(false);
@@ -23,14 +39,41 @@ public class ListActivity extends AppCompatActivity {
         actionBar.setDisplayShowCustomEnabled(true);
 
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String userId = user.getUid();
+
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.planets_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new OnSpinnerItemSelected());
+
+
+
+
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 
 
-    public class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
+    public class OnSpinnerItemSelected implements AdapterView.OnItemSelectedListener {
 
         public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
             // An item was selected. You can retrieve the selected item using
