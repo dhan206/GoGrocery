26a5fe8c1448e12b335d65a4624eee09cbf4d280
yongjp94 <yongjun.park.94@gmail.com>
2017-05-22 13:22:20 -0700

    Connected home to Sign-Up and changed buttons to purple

--- GoGrocery/app/src/main/java/edu/uw/dhan206/gogrocery/MainActivity.java
+++ GoGrocery/app/src/main/java/edu/uw/dhan206/gogrocery/MainActivity.java
@@ -1,13 +1,28 @@
 package edu.uw.dhan206.gogrocery;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View signUpButton = findViewById(R.id.signUpButton);
+        signUpButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case (R.id.signUpButton):
+                Intent signup = new Intent(MainActivity.this, CreateAccountActivity.class);
+                startActivity(signup);
+                break;
+        }
     }
 }
