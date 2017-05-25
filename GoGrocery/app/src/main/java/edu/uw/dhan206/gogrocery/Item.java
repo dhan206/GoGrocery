3ea5c907efc8a26f09a4c50f290fc98ce0c2ddd8
Colin Bartels <colin.0011@hotmail.com>
2017-05-24 17:22:13 -0700

    changed location stuff

--- GoGrocery/app/src/main/java/edu/uw/dhan206/gogrocery/Item.java
+++ GoGrocery/app/src/main/java/edu/uw/dhan206/gogrocery/Item.java
@@ -1,10 +1,9 @@
 package edu.uw.dhan206.gogrocery;
 
-import com.google.android.gms.location.places.Place;
-
 public class Item {
     public String name;
     public String description;
     public String addedBy;
-    public Place location;
+    public String locationName;
+    public String address;
 }
