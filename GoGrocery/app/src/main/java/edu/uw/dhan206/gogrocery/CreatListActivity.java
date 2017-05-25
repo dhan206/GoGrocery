a9e15bfcdc807d8644be2dbac0219a651790ada1
DereK Han <derekhan206@gmail.com>
2017-05-24 18:17:15 -0700

    Adds code to create new list

--- GoGrocery/app/src/main/java/edu/uw/dhan206/gogrocery/CreatListActivity.java
+++ GoGrocery/app/src/main/java/edu/uw/dhan206/gogrocery/CreatListActivity.java
@@ -1,7 +1,9 @@
 package edu.uw.dhan206.gogrocery;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -10,17 +12,34 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 
 import static android.R.attr.data;
 import static android.R.attr.name;
+import static android.R.attr.type;
 import static android.R.id.edit;
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
 
 public class CreatListActivity extends AppCompatActivity {
 
+    private final static String TAG = "CREATELISTACTIVITY";
     private ArrayAdapter<String> adapter;
-    private boolean isEditting = false;
+    private boolean isEditting;
     private ArrayList<String> newMembers;
+    private String groceryListID;
+    private String editType;
+    private FirebaseDatabase database;
+    private String userID;
+    private String currentListName;
+    private DatabaseReference currentListReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,17 +55,41 @@ public class CreatListActivity extends AppCompatActivity {
         final EditText groceryListName = (EditText) findViewById(R.id.createListName);
         final EditText newMemberField = (EditText) findViewById(R.id.createListNewMemberName);
 
+        Bundle extras = getIntent().getExtras();
+        if(extras != null) {
+            editType = extras.getString("Type");
+            groceryListID = extras.getString("Id");
+            isEditting = !(editType.equalsIgnoreCase("Edit"));
+            database = FirebaseDatabase.getInstance();
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            userID = user.getUid();
+            currentListReference = database.getReference("lists").child(groceryListID);
+            currentListReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    currentListName = dataSnapshot.child("name").getValue().toString();
+                    if(isEditting) {
+                        groceryListName.setText(currentListName);
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Log.v(TAG, "The read failed");
+                }
+            });
+//            Log.v(TAG, currentList.child("name").getValue().toString());
+        }
+
+
         if(isEditting) {
             TextView groceryListSubtext = (TextView) findViewById(R.id.createListNameSubtext);
             groceryListSubtext.setText("Edit name of grocery list");
 
             Button submitGroceryListButton = (Button) findViewById(R.id.createListSaveButton);
             submitGroceryListButton.setText("Save Grocery List");
-            groceryListName.setText("The Test Grocery List");
 
-            for (int i = 0; i < 5; i++) {
-                adapter.add("Testing " + i);
-            }
+
         } else {
             newMembers = new ArrayList<String>();
             findViewById(R.id.createListLeaveGroupButton).setVisibility(View.INVISIBLE);
@@ -59,7 +102,7 @@ public class CreatListActivity extends AppCompatActivity {
                 String newMemberEmail = newMemberField.getText().toString();
                 if(isEditting) {
                     // add member to firebase
-                    Toast.makeText(CreatListActivity.this, "New member added successfull.",
+                    Toast.makeText(CreatListActivity.this, "New member added successfully.",
                             Toast.LENGTH_SHORT).show();
                 } else {
                     newMembers.add(newMemberEmail);
@@ -73,14 +116,33 @@ public class CreatListActivity extends AppCompatActivity {
         saveGroceryList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (isEditting) {
+                String newNameInputField = groceryListName.getText().toString();
+                if (!newNameInputField.isEmpty()) {
+                    if (isEditting) {
+                        if (!currentListName.equals(newNameInputField)) {
+                            currentListReference.child("name").setValue(newNameInputField);
+                        }
+                    } else {
+                        DatabaseReference newList = database.getReference("lists").push();
+                        newList.child("name").setValue(newNameInputField);
+                        newList.child("users");
+                        newList.child("items");
 
-                } else {
-                    for(String member : newMembers) {
-                        // add each member to firebase
+                        DatabaseReference userReference = database.getReference("users").child(userID).child("lists");
+                        userReference.child(newNameInputField).setValue(newList.getKey());
+
+                        for (String member : newMembers) {
+
+                        }
+                        Toast.makeText(CreatListActivity.this, "All members added successfully.",
+                                Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(CreatListActivity.this, ListActivity.class));
+                        Toast.makeText(CreatListActivity.this, "New List Added",
+                                Toast.LENGTH_SHORT).show();
                     }
-                    Toast.makeText(CreatListActivity.this, "All members added successfully.",
-                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(CreatListActivity.this, "Please input a name for the grocery list.",
+                            Toast.LENGTH_LONG).show();
                 }
             }
         });
