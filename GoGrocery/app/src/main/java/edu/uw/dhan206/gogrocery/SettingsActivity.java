2568dee212f85bdb55f1246fb78071baecefb76e
Joseph Lee <yoseb123@hotmail.com>
2017-05-29 13:54:10 -0700

    settings activity

--- GoGrocery/app/src/main/java/edu/uw/dhan206/gogrocery/SettingsActivity.java
+++ GoGrocery/app/src/main/java/edu/uw/dhan206/gogrocery/SettingsActivity.java
@@ -1,13 +1,30 @@
 package edu.uw.dhan206.gogrocery;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * Created by joseph on 2017-05-22.
  */
 
+
+
+
+
 public class SettingsActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private final String TAG = "SettingsActivity";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +36,16 @@ public class SettingsActivity extends AppCompatActivity {
 
         ft.replace(android.R.id.content, settingsFragment, "SettingsFragment");
         ft.commit();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        final Button button = (Button) findViewById(R.id.logOutButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
