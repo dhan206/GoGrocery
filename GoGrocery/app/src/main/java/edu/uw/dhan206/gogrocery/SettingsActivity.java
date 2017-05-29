8a45e6d84d4028020da2b90afed14a294db8b2df
yongjp94 <yongjun.park.94@gmail.com>
2017-05-29 14:28:37 -0700

    setting activity bug fixed

--- GoGrocery/app/src/main/java/edu/uw/dhan206/gogrocery/SettingsActivity.java
+++ GoGrocery/app/src/main/java/edu/uw/dhan206/gogrocery/SettingsActivity.java
@@ -2,14 +2,10 @@ package edu.uw.dhan206.gogrocery;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 /**
@@ -37,7 +33,9 @@ public class SettingsActivity extends AppCompatActivity {
             public void onClick(View v) {
                 mAuth.signOut();
                 Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
+                finish();
             }
         });
     }
